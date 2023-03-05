5d35d276af2b24ff380bc6d2fa082f3370e818b4
rachon34 <rachon3441@gmail.com>
2023-03-05 14:49:21 +0100

    mise en place des 5 pieces .

--- pieces.js
+++ pieces.js
@@ -2,7 +2,10 @@
 const reponse = await fetch('pieces-autos.json');
 const pieces = await reponse.json();
 // Création des balises 
-const article = pieces[0];
+for (let i=0; i < pieces.length; i++){
+const sectionFiches = document.querySelector(".fiches");
+const article = pieces[i];
+const pieceElement = document.createElement("article");
 const imageElement = document.createElement("img");
 imageElement.src = article.image;
 const nomElement = document.createElement("h2");
@@ -11,15 +14,22 @@ const prixElement = document.createElement("p");
 prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
 const categorieElement = document.createElement("p");
 categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
+
+//Création des élements pour l'exercice
 const descriptionElement = document.createElement("p");
-descriptionElement.innerText = article.description ?? "(Pas de description pour le moment)";
-const stockStatElement = document.createElement("p");
-stockStatElement.innerText = `Disponibilité: ${article.disponibilité = "oui" ? "En stock" : "Rupture de stock"} `; 
+descriptionElement.innerText = article.description ?? "Pas de description pour le moment.";
+
+const stockElement = document.createElement("p");
+stockElement.innerText = `Disponibilité: ${article.disponibilité != "non" ? "En stock" : "Rupture de stock"}`;
+
 //Rattachement de nos balises au DOM
-const sectionFiches = document.querySelector(".fiches");
-sectionFiches.appendChild(imageElement);
-sectionFiches.appendChild(nomElement);
-sectionFiches.appendChild(prixElement);
-sectionFiches.appendChild(categorieElement);
-sectionFiches.appendChild(descriptionElement);
-sectionFiches.appendChild(stockStatElement);    
+sectionFiches.appendChild(pieceElement)
+pieceElement.appendChild(imageElement);
+pieceElement.appendChild(nomElement);
+pieceElement.appendChild(prixElement);
+pieceElement.appendChild(categorieElement);
+pieceElement
+//Rattachement des èléments au DOM pour l'exercice
+pieceElement.appendChild(descriptionElement);
+pieceElement.appendChild(stockElement)
+}
